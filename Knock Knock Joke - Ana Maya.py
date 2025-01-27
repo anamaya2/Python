374e17de2f0c6dd9a9802d2aef9ebba0e1898bcb
anamaya2 <anamayaa4@gmail.com>
2025-01-27 14:44:39 -0600

    Add files via upload

---
+++ Knock Knock Joke - Ana Maya.py
@@ -0,0 +1,5 @@
+# Name: Ana Maya
+# GitHub Link: https://github.com/anamaya2/Python
+# Program prints out a Knock Knock Joke
+
+print("Knock, knock.\nWho's there?\nLettuce\nLettuce who?\nLettuce in and you'll find out!")
