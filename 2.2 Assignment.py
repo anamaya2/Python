20ee2284a1762b3a78f83b0a7f6b71c3268fb96d
anamaya2 <anamayaa4@gmail.com>
2025-01-27 15:54:23 -0600

    Add files via upload

---
+++ 2.2 Assignment.py
@@ -0,0 +1,27 @@
+
+# This is a Python program to demonstrate different data types
+
+#Candy Shop Inventory
+# Starting Number of Snickers
+x = 25
+print("Snickers Available:", x)
+
+# Snickers Sold
+y = -6
+print("Snickers Sold:", y)
+
+# Remaining Snickers
+w = x+y
+print("Snickers Remaining:", w)
+
+# Money Made
+z = 18.50
+print("Money Made: $", z)
+
+# Store Name
+name = "Ana's Candy Shop"
+print("Store:", name)
+
+# Sold Over 5 Candy Bars
+is_active = True
+print("Sold over 5 Candy Bars?:", is_active)
