7f9e72795a2b922cb972e31882c6076ceb6948f0
anamaya2 <anamayaa4@gmail.com>
2025-02-03 18:25:11 -0600

    Add files via upload

---
+++ homework2 - Ana Maya.py
@@ -0,0 +1,73 @@
+'''
+Homework2
+Name: Ana Maya
+github link: 
+'''
+
+in_value_1 = 2.5
+in_value_2 = 6.2
+in_value_3 = 1.4
+#Conversion from inches to centimeters: 1 inch = 2.54 cm
+conversion_value = 2.54
+
+#Calculating the values in centimeters
+cm_value_1 = in_value_1 * conversion_value
+cm_value_2 = in_value_2 * conversion_value
+cm_value_3 = in_value_3 * conversion_value
+
+print(f"{in_value_1} inches is equal to {cm_value_1} centimeters")
+print(f"{in_value_2} inches is equal to {cm_value_2} centimeters")
+print(f"{in_value_3} inches is equal to {cm_value_3} centimeters")
+
+
+ft_value_1 = 5
+ft_value_2 = 3
+ft_value_3 = 7
+
+#Conversion from feet to meters: 1 foot =  0.3048 m
+conversion_value = 0.3048
+
+#Calculating the values in meters
+m_value_1 = ft_value_1 * conversion_value
+m_value_2 = ft_value_2 * conversion_value
+m_value_3 = ft_value_3 * conversion_value
+
+print(f"{ft_value_1} feet is equal to {m_value_1} meters")
+print(f"{ft_value_2} feet is equal to {m_value_2} meters")
+print(f"{ft_value_3} feet is equal to {m_value_3} meters")
+
+
+lb_value_1 = 150
+lb_value_2 = 90
+lb_value_3 = 60
+
+#Conversion from pounds to kilograms: 1 pound = 0.453592 kg
+conversion_value = 0.453592
+
+#Calculating the values in kilograms
+kg_value_1 = lb_value_1 * conversion_value
+kg_value_2 = lb_value_2 * conversion_value
+kg_value_3 = lb_value_3 * conversion_value
+
+print(f"{lb_value_1} pounds is equal to {kg_value_1} kilograms")
+print(f"{lb_value_2} pounds is equal to {kg_value_2} kilograms")
+print(f"{lb_value_3} pounds is equal to {kg_value_3} kilograms")
+
+
+hr_value_1 = 2
+hr_value_2 = 1
+hr_value_3 = 5
+
+#Conversion from hours to minutes: 1 hour = 60 minutes
+conversion_value = 60
+
+#Calculating the values in minutes
+min_value_1 = hr_value_1 * conversion_value
+min_value_2 = hr_value_2 * conversion_value
+min_value_3 = hr_value_3 * conversion_value
+
+print(f"{hr_value_1} hours is equal to {min_value_1} minutes")
+print(f"{hr_value_2} hours is equal to {min_value_2} minutes")
+print(f"{hr_value_3} hours is equal to {min_value_3} minutes")
+
+
