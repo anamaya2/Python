d79eaf372606fa44a97ae5315302259c58bd2859
anamaya2 <anamayaa4@gmail.com>
2025-02-10 23:37:47 -0600

    Update homework2 - Ana Maya.py

--- homework2 - Ana Maya.py
+++ homework2 - Ana Maya.py
@@ -1,73 +1,31 @@
 '''
 Homework2
 Name: Ana Maya
-github link: 
+github link: https://github.com/anamaya2/Python/edit/main/homework2%20-%20Ana%20Maya.py
 '''
 
-in_value_1 = 2.5
-in_value_2 = 6.2
-in_value_3 = 1.4
-#Conversion from inches to centimeters: 1 inch = 2.54 cm
-conversion_value = 2.54
+def inches_to_cm(inches):
+    conversion = 2.54
+    cm_value = inches * conversion
+    return cm_value
 
-#Calculating the values in centimeters
-cm_value_1 = in_value_1 * conversion_value
-cm_value_2 = in_value_2 * conversion_value
-cm_value_3 = in_value_3 * conversion_value
+def feet_to_meters(feet):
+    conversion = 0.3048
+    meters_value = feet * conversion
+    return round(meters_value, 2)
 
-print(f"{in_value_1} inches is equal to {cm_value_1} centimeters")
-print(f"{in_value_2} inches is equal to {cm_value_2} centimeters")
-print(f"{in_value_3} inches is equal to {cm_value_3} centimeters")
+def lbs_to_kg(lbs):
+    conversion = 0.453592
+    kg_value = lbs * conversion
+    return round(kg_value, 2)
 
+def hours_to_minutes(hrs):
+    conversion = 60
+    minutes_value = hrs * conversion
+    return minutes_value
 
-ft_value_1 = 5
-ft_value_2 = 3
-ft_value_3 = 7
-
-#Conversion from feet to meters: 1 foot =  0.3048 m
-conversion_value = 0.3048
-
-#Calculating the values in meters
-m_value_1 = ft_value_1 * conversion_value
-m_value_2 = ft_value_2 * conversion_value
-m_value_3 = ft_value_3 * conversion_value
-
-print(f"{ft_value_1} feet is equal to {m_value_1} meters")
-print(f"{ft_value_2} feet is equal to {m_value_2} meters")
-print(f"{ft_value_3} feet is equal to {m_value_3} meters")
-
-
-lb_value_1 = 150
-lb_value_2 = 90
-lb_value_3 = 60
-
-#Conversion from pounds to kilograms: 1 pound = 0.453592 kg
-conversion_value = 0.453592
-
-#Calculating the values in kilograms
-kg_value_1 = lb_value_1 * conversion_value
-kg_value_2 = lb_value_2 * conversion_value
-kg_value_3 = lb_value_3 * conversion_value
-
-print(f"{lb_value_1} pounds is equal to {kg_value_1} kilograms")
-print(f"{lb_value_2} pounds is equal to {kg_value_2} kilograms")
-print(f"{lb_value_3} pounds is equal to {kg_value_3} kilograms")
-
-
-hr_value_1 = 2
-hr_value_2 = 1
-hr_value_3 = 5
-
-#Conversion from hours to minutes: 1 hour = 60 minutes
-conversion_value = 60
-
-#Calculating the values in minutes
-min_value_1 = hr_value_1 * conversion_value
-min_value_2 = hr_value_2 * conversion_value
-min_value_3 = hr_value_3 * conversion_value
-
-print(f"{hr_value_1} hours is equal to {min_value_1} minutes")
-print(f"{hr_value_2} hours is equal to {min_value_2} minutes")
-print(f"{hr_value_3} hours is equal to {min_value_3} minutes")
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest2.py'))
 
 
