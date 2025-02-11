26188c67b056995678bd40d7f0a7c5c9ca576987
anamaya2 <anamayaa4@gmail.com>
2025-02-10 23:22:15 -0600

    Add files via upload

---
+++ homework8-AnaMaya.py
@@ -0,0 +1,25 @@
+'''
+Homework8
+Name: Ana Maya
+github link:
+'''
+
+def sum_numbers(lst):
+    sum = 0
+    # Create for loop to iterate through the list
+    for num in lst:
+        sum+= num
+    return sum
+
+def largest_number(lst):
+    largest = lst[0]
+    # Create for loop to iterate through the list
+    for num in lst:
+        # Check if the current number is greater than the largest number found so far
+        if num > largest:
+            largest = num
+    return largest
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest8.py'))
