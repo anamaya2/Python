f1804e50aceac39b5926faef467bce085a5ce13e
anamaya2 <anamayaa4@gmail.com>
2025-02-24 17:42:18 -0600

    Add files via upload

---
+++ homework13.py
@@ -0,0 +1,18 @@
+'''
+Homework13
+Name: Ana Maya
+github link:
+'''
+def calculate_interest(principal, rate, time):
+    interest = (principal * rate * time)
+    return int(interest)
+
+def compound_interest(principal, rate, n, time, ):
+    amount = principal * (1 + rate / n) ** (n * time)
+    compound_interest = amount - principal
+    return (round(compound_interest,2))
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest13.py'))
