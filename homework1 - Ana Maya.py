7bfa9cddb8135695bd2bcc4fc77a515b4710f7ea
anamaya2 <anamayaa4@gmail.com>
2025-03-10 18:25:27 -0500

    Add files via upload

---
+++ homework1 - Ana Maya.py
@@ -0,0 +1,36 @@
+'''
+Homework1
+Name: Ana Maya
+github link: 
+'''
+
+import random
+
+def check(guess, actual_number):
+    # Getting the difference between the guess and the actual number
+    difference = abs(guess - actual_number)
+    # Returning the appropriate text based on the guess
+    if difference == 0:
+        return "You Got It!"
+    elif difference < 5:
+        return "Very Hot"
+    elif difference < 10:
+        return "Hot"
+    elif difference < 20:
+        return "Cool"
+    else:
+        return "Cold"
+    pass
+    
+def main(actual_number):
+    actual_number = random.randint(1,100)
+    
+    while True:
+        guess = int(input("Guess a number between 1 and 100: "))
+        print(check(guess, actual_number))
+        if check(guess, actual_number) == "You Got It!":
+            break
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest1.py'))
