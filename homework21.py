a73cf2dd9e5191308d1267833170023510e70568
anamaya2 <anamayaa4@gmail.com>
2025-03-17 20:50:02 -0500

    Add files via upload

---
+++ homework21.py
@@ -0,0 +1,17 @@
+'''
+Homework21
+Name: Ana Maya
+github link: 
+'''
+def is_palindrome(string):
+    cleaned_string = ''.join(char.lower() for char in string if char.isalnum())
+    return cleaned_string == cleaned_string[::-1]
+    
+def is_anagram(string1,string2):
+    cleaned_string1 = ''.join(char.lower() for char in string1 if char.isalnum())
+    cleaned_string2 = ''.join(char.lower() for char in string2 if char.isalnum())
+    return sorted(cleaned_string1) == sorted(cleaned_string2)
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest21.py'))
