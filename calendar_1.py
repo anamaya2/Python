8275575827e13fbd53310950df67d7f83a239679
anamaya2 <anamayaa4@gmail.com>
2025-04-28 22:16:09 -0500

    Add files via upload

---
+++ calendar_1.py
@@ -0,0 +1,25 @@
+'''
+Name: Ana Maya
+GitHub Link:
+'''
+
+import calendar
+import datetime
+
+def main():
+
+    current_year = datetime.datetime.now().year
+
+    while True:
+        try:
+            month = int(input("Enter your birth month (1-12): "))
+
+            if 1<= month <= 12:
+                print(f"Your birth month calendar is {calendar.month(current_year, month)}")
+                break
+            else:
+                print("Please enter a valid month (1-12)")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 12.")
+
+main()
